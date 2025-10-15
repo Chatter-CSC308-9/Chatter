46c25b854f69e5355e7deb97fbce3cb2755b1979
KnowingPigeon <67757823+KnowingPigeon@users.noreply.github.com>
2025-10-15 19:06:30 +0200

    Removed Main application, replaced with a general window wrapper

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,5 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.Button;
-
-public class Main extends Application {
-
-    Label label1;
-    Button button1;
-    int i = 1;
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("Chatter");
-        label1 = new Label("Behold the VBox");
-        VBox root = new VBox();
-
-        Scene scene = new Scene(root, 400, 400);
-        stage.setScene(scene);
-
-        button1 = new Button("Push me");
-        button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                label1.setText("Pushes: " + i);
-                i++;
-            }
-        });
-
-        root.getChildren().addAll(label1, button1);
-        stage.show();
+        FXBuilderHome.main(args);
     }
 }
