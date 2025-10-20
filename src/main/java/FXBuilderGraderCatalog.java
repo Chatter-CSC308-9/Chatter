874c828cbd6c61e034c8e1b3cb9ad305bcc78aff
Chandni <csharm08@calpoly.edu>
2025-10-20 10:47:22 +0200

    Added Grader Catalog(where they can find assignments to grade)

---
+++ src/main/java/FXBuilderGraderCatalog.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FXBuilderGraderCatalog extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/GraderCatalog.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Chatter - FXML UI");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
