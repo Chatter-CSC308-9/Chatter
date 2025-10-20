16e21a2c5407d1176aabc94c0a96e0132235412b
KnowingPigeon <67757823+KnowingPigeon@users.noreply.github.com>
2025-10-20 12:26:57 +0200

    Simplified Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,6 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        args = new String[1];
-        args[0] = "GraderCatalog";
-        Application.launch(WindowLauncher.class, args);
+        Application.launch(WindowLauncher.class, "Login");
     }
 }
