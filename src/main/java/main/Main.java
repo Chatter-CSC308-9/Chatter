cdadc63952edecc8d7f87e91c2898fc77b2f171b
KnowingPigeon <67757823+KnowingPigeon@users.noreply.github.com>
2025-10-23 11:44:31 +0200

    Refactored UI naming conventions, removed duplicates

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,6 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        Application.launch(WindowLauncher.class, "Home");
+        Application.launch(WindowLauncher.class, "Feedback");
     }
 }
