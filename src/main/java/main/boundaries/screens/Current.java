c27a8eb6a3fbdde04cddb91d0f0840945cb14bab
m33per <mibawesome1@gmail.com>
2025-10-25 16:40:26 +0200

    added learner taskbar navigation closes #13

--- src/main/java/main/boundaries/screens/Current.java
+++ src/main/java/main/boundaries/screens/Current.java
@@ -1,44 +1,24 @@
 package main.boundaries.screens;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import main.Main;
+import main.boundaries.Navigator;
+import main.boundaries.ShellAPI;
 
-import java.io.IOException;
-import java.util.Objects;
+public class Current implements Navigator {
+    @FXML
+    public Button loginButton;
 
-public class Current {
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
+    private ShellAPI shellAPI;
 
     @FXML
-    private AnchorPane currentAnchorPane;
+    private void handleButtonClick() {
+        shellAPI.setTaskbar("LearnerTaskbar");
+        shellAPI.setContent("ChatterHome");
+    }
 
-    @FXML
-    private Button perryThePlatypus;
-
-    public void switchToCurrentEdit(ActionEvent event) throws IOException {
-        System.out.println("Clicked perry the platypus");
-        /*Parent root = FXMLLoader.load(getClass().getResource("CurrentEdit.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();*/
-        /*AnchorPane nextAnchorPane = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("screens/CurrentEdit")));
-        currentAnchorPane.getChildren().removeAll();
-        currentAnchorPane.getChildren().setAll(nextAnchorPane);*/
-        Parent currentEditParent = FXMLLoader.load(getClass().getResource("main/resources/ui/screens/CurrentEdit.fxml"));
-        Scene currentEditScene = new Scene(currentEditParent);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setScene(currentEditScene);
-        window.show();
+    @Override
+    public void setShellAPI(ShellAPI shellAPI) {
+        this.shellAPI = shellAPI;
     }
 }
