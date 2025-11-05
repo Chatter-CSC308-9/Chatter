76bb0be568d9ee4dcbf82b7798c23674ece76794
KnowingPigeon <67757823+KnowingPigeon@users.noreply.github.com>
2025-11-05 12:05:35 +0100

    Refactored Account boundaries to reduce code duplication

---
+++ src/main/java/main/boundaries/screens/AbstractAccount.java
@@ -0,0 +1,41 @@
+package main.boundaries.screens;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import main.boundaries.Boundary;
+import main.boundaries.apis.hooks.ShellNavigateAPI;
+import main.boundaries.apis.interfaces.Navigator;
+import main.controllers.DisplayUsernameController;
+import main.controllers.LogoutController;
+
+public abstract class AbstractAccount extends Boundary implements Navigator {
+
+    @FXML
+    public Label greetingLabel;
+    @FXML
+    public Button logoutButton;
+
+    LogoutController logoutController;
+    DisplayUsernameController displayUsernameController;
+
+    private ShellNavigateAPI shellNavigateAPI;
+
+    public void handleLogoutButtonClick() {
+        this.logoutController.logout();
+        shellNavigateAPI.setContent("Login");
+        shellNavigateAPI.setTaskbar();
+    }
+
+    @Override
+    public void setNavigateAPI(ShellNavigateAPI shellNavigateAPI) {
+        this.shellNavigateAPI = shellNavigateAPI;
+    }
+
+    @FXML
+    @Override
+    public void onShow() {
+        greetingLabel.setText("Hi " + displayUsernameController.getUsername() + "!");
+    }
+
+}
--- src/main/java/main/boundaries/screens/GraderAccount.java
+++ src/main/java/main/boundaries/screens/GraderAccount.java
@@ -1,48 +1,16 @@
 package main.boundaries.screens;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import main.boundaries.Boundary;
-import main.boundaries.apis.hooks.ShellNavigateAPI;
 import main.boundaries.apis.interfaces.Navigator;
 import main.controllers.DisplayUsernameController;
 import main.controllers.LogoutController;
 
-public class GraderAccount extends Boundary implements Navigator {
-
-    @FXML
-    public Button logoutButton;
-    @FXML
-    public Label greetingLabel;
-
-    LogoutController logoutController;
-    DisplayUsernameController displayUsernameController;
-
-    private ShellNavigateAPI shellNavigateAPI;
+public class GraderAccount extends AbstractAccount implements Navigator {
 
     public GraderAccount(LogoutController logoutController, DisplayUsernameController displayUsernameController) {
         this.logoutController = logoutController;
         this.displayUsernameController = displayUsernameController;
         super.addController(this.logoutController);
         super.addController(this.displayUsernameController);
-    }
-
-    public void handleLogoutButtonClick() {
-        this.logoutController.logout();
-        shellNavigateAPI.setContent("Login");
-        shellNavigateAPI.setTaskbar();
-    }
-
-    @Override
-    public void setNavigateAPI(ShellNavigateAPI shellNavigateAPI) {
-        this.shellNavigateAPI = shellNavigateAPI;
-    }
-
-    @FXML
-    @Override
-    public void onShow() {
-        greetingLabel.setText("Hi " + displayUsernameController.getUsername() + "!");
     }
 
 }
