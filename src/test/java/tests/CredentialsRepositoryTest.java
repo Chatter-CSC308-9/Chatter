9349b95c04650bd9a8fd337e40456b94e28607b1
KnowingPigeon <67757823+KnowingPigeon@users.noreply.github.com>
2025-11-25 08:41:57 +0100

    implemented loop test 2

--- src/test/java/tests/CredentialsRepositoryTest.java
+++ src/test/java/tests/CredentialsRepositoryTest.java
@@ -5,9 +5,7 @@ import main.entities.UserCredentials;
 import org.junit.Test;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -16,9 +14,7 @@ public class CredentialsRepositoryTest {
     @Test
     public void getAllUserCredentialsTest() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         // create testing CredentialsRepository
-        Constructor<CredentialsRepository> testingConstructor = CredentialsRepository.class.getDeclaredConstructor(boolean.class);
-        testingConstructor.setAccessible(true);
-        var credentialsRepository = testingConstructor.newInstance(true);
+        var credentialsRepository = getTestingRepository();
 
         // 0 loops, no users
         TestingServerUtil.clearTestServer();
@@ -27,7 +23,7 @@ public class CredentialsRepositoryTest {
         assertEquals(expected, result);
 
         // 1 loop, 1 user
-        expected.add(new UserCredentials("a", "b", 1L, false));
+        expected.add(generateRandomUserCredential());
         for (UserCredentials userCredentials : expected) {
             credentialsRepository.addUserCredential(userCredentials);
         }
@@ -36,7 +32,7 @@ public class CredentialsRepositoryTest {
 
         // 2 loops, 2 users
         TestingServerUtil.clearTestServer();
-        expected.add(new UserCredentials("b", "a", 2L, true));
+        expected.add(generateRandomUserCredential());
         for (UserCredentials userCredentials : expected) {
             credentialsRepository.addUserCredential(userCredentials);
         }
@@ -46,7 +42,7 @@ public class CredentialsRepositoryTest {
         // 50 loops, 50 users
         TestingServerUtil.clearTestServer();
         for (int i = 0; i < 48; i++) {
-            expected.add(new UserCredentials("a", "a", (long) i, false));
+            expected.add(generateRandomUserCredential());
         }
         for (UserCredentials userCredentials : expected) {
             credentialsRepository.addUserCredential(userCredentials);
@@ -56,4 +52,81 @@ public class CredentialsRepositoryTest {
 
         TestingServerUtil.clearTestServer();
     }
+
+    @Test
+    public void getUserCredentialsTest() throws InvocationTargetException, NoSuchMethodException,
+            InstantiationException, IllegalAccessException {
+        var credentialsRepository = getTestingRepository();
+        UserCredentials credentialToFind = new UserCredentials("perry", "", 1L, false);
+        String usernameToGet = credentialToFind.username;
+
+        // No credentials exist
+        setupTest();
+        assertEquals(Optional.empty(), credentialsRepository.getUserCredentials(usernameToGet));
+
+        // User is first credential
+        setupTest(credentialsRepository, credentialToFind);
+        assertEquals(Optional.of(credentialToFind),credentialsRepository.getUserCredentials(usernameToGet));
+
+        // User is second credential
+        setupTest(credentialsRepository, 1, credentialToFind);
+        assertEquals(Optional.of(credentialToFind),credentialsRepository.getUserCredentials(usernameToGet));
+
+        // User is in the middle of many credentials
+        setupTest(credentialsRepository, 50, credentialToFind, 50);
+        assertEquals(Optional.of(credentialToFind),credentialsRepository.getUserCredentials(usernameToGet));
+
+        // User is second-to-last credential
+        setupTest(credentialsRepository, 50, credentialToFind, 1);
+        assertEquals(Optional.of(credentialToFind),credentialsRepository.getUserCredentials(usernameToGet));
+
+        // User is last credential
+        setupTest(credentialsRepository, 50, credentialToFind);
+        assertEquals(Optional.of(credentialToFind),credentialsRepository.getUserCredentials(usernameToGet));
+
+        TestingServerUtil.clearTestServer();
+    }
+
+    private void setupTest(CredentialsRepository credentialsRepository, int before, UserCredentials target, int after) {
+        TestingServerUtil.clearTestServer();
+        addRandomCredentials(credentialsRepository, before);
+        if (target != null) {
+            credentialsRepository.addUserCredential(target);
+        }
+        addRandomCredentials(credentialsRepository, after);
+    }
+
+    private void setupTest(CredentialsRepository credentialsRepository, UserCredentials target) {
+        setupTest(credentialsRepository, 0, target, 0);
+    }
+
+    private void setupTest(CredentialsRepository credentialsRepository, int before, UserCredentials target) {
+        setupTest(credentialsRepository, before, target, 0);
+    }
+
+    private void setupTest() {
+        TestingServerUtil.clearTestServer();
+    }
+
+    private void addRandomCredentials(CredentialsRepository credentialsRepository, int count) {
+        for (int i = 0; i < count; i++) {
+            credentialsRepository.addUserCredential(generateRandomUserCredential());
+        }
+    }
+
+    private CredentialsRepository getTestingRepository() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Constructor<CredentialsRepository> testingConstructor = CredentialsRepository.class.getDeclaredConstructor(boolean.class);
+        testingConstructor.setAccessible(true);
+        return testingConstructor.newInstance(true);
+    }
+
+    private UserCredentials generateRandomUserCredential() {
+        var random = new Random();
+        String username = Double.toString(Math.random());
+        String password = Double.toString(Math.random());
+        long userId = random.nextLong();
+        boolean isGrader = Math.random() < 0.5;
+        return new UserCredentials(username, password, userId, isGrader);
+    }
+
 }
